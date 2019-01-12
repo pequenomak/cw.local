88031512d021e7cca7728370bde0fbd6cad367c3
pequenomak <pequeno.mak@gmail.com>
2019-01-12 19:45:01 +0600

    backup initial

---
+++ data/about_data.php
@@ -0,0 +1,44 @@
+<?php
+ return [
+     'about' => [
+         'name' => 'Сергей Макаров',
+         'photo' => '',
+         'email' => 'serejka.makarov@gmail.com',
+         'phone' => '+7(953)399-60-69',
+         'site' => 'example.com',
+         'post' => 'Php developer'
+     ],
+     'education' => [
+        [
+            'title' => 'Школа',
+            'speciality' => 'школьник',
+            'yearStart' => '2005',
+            'yearEnd' => '2016',
+        ],
+        [
+            'title' => 'ОмГУПС',
+            'speciality' => 'студент',
+            'yearStart' => '2016',
+            'yearEnd' => '2020',
+        ]
+     ],
+     'languages' => [
+         [
+             'title' => 'English',
+             'level' => 'beginner'
+         ],
+         [
+             'title' => 'Portugal',
+             'level' => 'beginner'
+         ],
+         [
+             'title' => 'Russian',
+             'level' => 'Native'
+         ]
+     ],
+     'interests' => [
+         'Музыка',
+         'Капоэйра',
+         'Фильмы'
+     ]
+ ];
---
+++ data/career_data.php
@@ -0,0 +1,91 @@
+<?php
+ return [
+     'about' => 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+     'education' => [
+        [
+            'title' => 'Школа',
+            'speciality' => 'школьник',
+            'yearStart' => '2005',
+            'yearEnd' => '2016',
+        ],
+        [
+            'title' => 'ОмГУПС',
+            'speciality' => 'студент',
+            'yearStart' => '2016',
+            'yearEnd' => '2020',
+        ]
+     ],
+     'experiences' => [
+         [
+             'title' => 'Место работы 1',
+             'post' => 'Должность 1',
+             'city' => 'Омск',
+             'company' => 'Компания1',
+             'about' => 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+             'yearStart' => '2016',
+             'yearEnd' => '2020'
+         ],
+         [
+             'title' => 'Место работы 2',
+             'post' => 'Должность 2',
+             'city' => 'Омск',
+             'company' => 'Компания2',
+             'about' => 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+             'yearStart' => '2016',
+             'yearEnd' => '2020'
+         ],
+         [
+             'title' => 'Место работы 3',
+             'post' => 'Должность 3',
+             'city' => 'Омск',
+             'company' => 'Компания3',
+             'about' => 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+             'yearStart' => '2016',
+             'yearEnd' => '2020'
+         ],
+         [
+             'title' => 'Место работы 4',
+             'post' => 'Должность 4',
+             'city' => 'Омск',
+             'company' => 'Компания4',
+             'about' => 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+             'yearStart' => '2016',
+             'yearEnd' => '2020'
+         ]
+     ],
+     'projects' => [
+         [
+             'title' => 'project 1',
+             'link' => 'https://example.com',
+             'about' => 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.'
+         ],
+         [
+             'title' => 'project 2',
+             'link' => 'https://example2.com',
+             'about' => 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.'
+         ],
+         [
+             'title' => 'project 3',
+             'link' => 'https://example3.com',
+             'about' => 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.'
+         ],
+     ],
+     'skills' => [
+         [
+             'title' => 'PHP',
+             'level' => '90'
+         ],
+         [
+             'title' => 'html & css',
+             'level' => '100'
+         ],
+         [
+             'title' => 'JS',
+             'level' => '75'
+         ],
+         [
+             'title' => 'C & C++',
+             'level' => '55'
+         ]
+     ]
+ ];
